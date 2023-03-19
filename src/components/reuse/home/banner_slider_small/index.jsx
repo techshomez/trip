607674b8235fb55211d27df943d8a7a86d6879ee
Daniel Tope <danieltopeg@gmail.com>
2023-03-19 14:14:34 +0100

    slider components

--- src/components/reuse/home/banner_slider_small/index.jsx
+++ src/components/reuse/home/banner_slider_small/index.jsx
@@ -1,10 +1,41 @@
+import Slider from "infinite-react-carousel";
 import "./banner_slider.css";
 
 const BannerSlider = () => {
   return (
     <div className="banner_slider">
       <div className="container">
-        <h2>banner slider</h2>
+        <div className="img_left">
+          <img src="/assets/banner/banner-sm.webp" alt="" />
+        </div>
+        <div className="slider_right">
+          <Slider dots>
+            <div>
+              <img src="/assets/slider/1.webp" alt="" />
+            </div>
+            <div>
+              <img src="/assets/slider/2.webp" alt="" />
+            </div>
+            <div>
+              <img src="/assets/slider/3.webp" alt="" />
+            </div>
+            <div>
+              <img src="/assets/slider/4.webp" alt="" />
+            </div>
+            <div>
+              <img src="/assets/slider/5.webp" alt="" />
+            </div>
+            <div>
+              <img src="/assets/slider/6.webp" alt="" />
+            </div>
+            <div>
+              <img src="/assets/slider/7.webp" alt="" />
+            </div>
+            <div>
+              <img src="/assets/slider/8.webp" alt="" />
+            </div>
+          </Slider>
+        </div>
       </div>
     </div>
   );
