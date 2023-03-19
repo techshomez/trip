2858dca50d798a8ee90030f54b2938801fee204f
Daniel Tope <danieltopeg@gmail.com>
2023-03-19 18:19:08 +0100

    property cards

--- src/components/reuse/cardlist/index.jsx
+++ src/components/reuse/cardlist/index.jsx
@@ -1,10 +1,91 @@
-import React from "react";
-
+import { FaStar } from "react-icons/fa";
+import "./cardlist.css";
 const CardList = () => {
   return (
     <div className="cardlist">
-      <div className="container">
-        <h2>Bangkok</h2>
+      <div className="card_wrapper">
+        <div className="list_items">
+          <img src="/assets/card/card_image/2.jpg" alt="" />
+          <div className="list_box">
+            <h2>
+              Swissotel Bangkok Ratchada (SHA Extra Plus){" "}
+              <i>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </i>
+            </h2>
+            <div className="rating">
+              <span>
+                4.5 <small>/5</small>
+              </span>
+              <span>562 Reviews </span>
+            </div>
+            <div className="price">
+              <small>From</small> <span>US$87</span>
+            </div>
+          </div>
+        </div>
+        <div className="list_items">
+          <img src="/assets/card/card_image/1.jpg" alt="" />
+          <div className="list_box">
+            <h2>
+              Bangkok Marriott Marquis Queen's Park
+              <i>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </i>
+            </h2>
+            <div className="rating">
+              <span>
+                4.5 <small>/5</small>
+              </span>
+              <span>562 Reviews </span>
+            </div>
+            <div className="price">
+              <small>From</small> <span>US$87</span>
+            </div>
+          </div>
+        </div>
+        <div className="list_items">
+          <img src="/assets/card/card_image/4.jpg" alt="" />
+          <div className="list_box">
+            <h2>
+              Rosewood Bangkok
+              <i>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </i>
+            </h2>
+            <div className="rating">
+              <span>
+                4.5 <small>/5</small>
+              </span>
+              <span>562 Reviews </span>
+            </div>
+            <div className="price">
+              <small>From</small> <span>US$87</span>
+            </div>
+          </div>
+        </div>
+        <div className="lists_item">
+          <img src="/assets/card/card_image/5.png" alt="" />
+          <div className="img_content">
+            <h2>
+              Discover great deals on <br />
+              hotels around the world
+            </h2>
+            <button>Go Now</button>
+          </div>
+        </div>
       </div>
     </div>
   );
