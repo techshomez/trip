87a50625097031e6b113b38c6fe2878d0527b2d2
Daniel Tope <danieltopeg@gmail.com>
2023-03-21 21:25:43 +0100

    app-download component/ structure & styling

--- src/components/appDownload/index.jsx
+++ src/components/appDownload/index.jsx
@@ -5,9 +5,43 @@ const AppDownload = () => {
     <div className="app_download">
       <div className="container">
         <div className="app_left">
-          <img src="/assets/mobile.jpg" alt="" />
+          <div className="img_hold">
+            <img src="/assets/mobile.jpg" alt="" />
+          </div>
         </div>
-        <div className="app_right">hi</div>
+        <div className="app_right">
+          <div className="right_top">
+            <h2>Your all-in-one travel app.</h2>
+            <p>
+              Book flights, hotels, trains & rental cars anywhere in the world
+              in just seconds. Get real-time flight updates, travel info,
+              exclusive deals, and 30% more Trip Coins only on the app!
+            </p>
+          </div>
+          <div className="top_bottom">
+            <div className="bottom_form">
+              <div className="bottom_nav">
+                <span>SMS</span>
+                <span>Email</span>
+              </div>
+              <article>
+                Enter your phone number to receive a text with a link to
+                download the app.
+              </article>
+              <div className="form_group"></div>
+            </div>
+            <div className="divider">|</div>
+            <div className="bottom_img">
+              <div className="app_down">
+                <img src="assets/android.png" alt="" />
+                <img src="assets/ios.png" alt="" />
+              </div>
+              <div className="qrcode">
+                <img src="/assets/qrcode.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
