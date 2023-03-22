cb8309130ca418b74d0f9b5e8f2e20dfbded18ff
Daniel Tope <danieltopeg@gmail.com>
2023-03-22 22:34:54 +0100

    app-download component

--- src/components/appDownload/index.jsx
+++ src/components/appDownload/index.jsx
@@ -1,24 +1,11 @@
 import "./app_download.css";
-import { FaChevronDown } from "react-icons/fa";
+import { FaChevronDown, FaSearch } from "react-icons/fa";
+import { FiCheck, FiSearch } from "react-icons/fi";
 import { useState } from "react";
-import { ReactCountryDropdown } from "react-country-dropdown";
-import "react-country-dropdown/dist/index.css";
 
 const AppDownload = () => {
   const [formopen, setFormopen] = useState(false);
 
-  const handleSelect = (country) => {
-    console.log(country);
-    /* returns the details on selected country as an object
-    	{
-          name: "United States of America", 
-          code: "US", 
-          capital: "Washington, D.C.", 
-          region: "Americas", 
-          latlng: [38, -97]
-        }
-    */
-  };
   return (
     <div className="app_download">
       <div className="container">
@@ -47,17 +34,55 @@ const AppDownload = () => {
                 download the app.
               </article>
               <div className="form_group">
-                <span onClick={() => setFormopen(!formopen)}>
-                  (+242)
-                  <i>
-                    <FaChevronDown />
-                  </i>
+                <span>
+                  <small onClick={() => setFormopen(!formopen)}>
+                    (+242)
+                    <i>
+                      <FaChevronDown />
+                    </i>
+                  </small>
                   {formopen && (
                     <div className="form_option">
-                      <ReactCountryDropdown
-                        onSelect={handleSelect}
-                        countryCode="IN"
-                      />
+                      <div className="search_frm">
+                        <h3>Selected</h3>
+                        <span>
+                          Nigeria{" "}
+                          <i>
+                            <FiCheck />
+                          </i>
+                        </span>
+                        <div className="search_input">
+                          <i>
+                            <FiSearch />
+                          </i>
+                          <input
+                            type="text"
+                            name=""
+                            id=""
+                            placeholder="Country or region"
+                          />
+                        </div>
+                      </div>
+                      <div className="country_items">
+                        <div className="item_title">Top</div>
+                        <div className="item_list">
+                          <div className="list">
+                            <h5>Nigeria</h5> <h6>+234</h6>
+                          </div>
+                          <span className="list">
+                            <h5>Philippines</h5> <h6>+63</h6>
+                          </span>
+                          <span className="list">
+                            <h5>India</h5> <h6>+91</h6>
+                          </span>
+                          <span className="list">
+                            <h5>Thailand</h5> <h6>+66</h6>
+                          </span>
+                          <span className="list">
+                            <h5>United Kingdom</h5> <h6>+44</h6>
+                          </span>
+                        </div>
+                      </div>
                     </div>
                   )}
                 </span>
